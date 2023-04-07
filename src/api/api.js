7d9344ad366a16ab9ed9159bba4ba0aef2131de6
yangaoqi <2669526712@qq.com>
2023-04-07 11:30:38 +0800

    feat: "axios模块化封装"

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,13 @@
+/**
+ * 有两种使用axios的方式
+ * 第一种是 直接引用对应的模块 
+ * 例如 ：
+ *    import User from '@/api/user';
+ *    User.login()
+ * 
+ * 第二种就是引用该模块，
+ * 然后 再 使用方法，大体上差不多，建议用上一个
+ */
 const api = import.meta.globEager("./*Api.js");
 Object.keys(api).forEach((module) => {
   const moduleName = module.substring(2, module.length - 3);
--- src/api/caseApi.js
+++ src/api/caseApi.js
@@ -4,15 +4,17 @@ import request from "./request";
  * case 模块
  */
 
-export default class Case {
-  /**
-   *
-   */
-  wuhu(data) {
-    return request({
-      method: "post",
-      url: "/wuhu",
-      data: data,
-    });
-  }
+/**
+ * 在这里写具体的方法
+ */
+function wuhu(data) {
+  return request({
+    method: "post",
+    url: "/wuhu",
+    data: data,
+  });
+}
+// 在这里导出
+export default {
+  wuhu
 }
